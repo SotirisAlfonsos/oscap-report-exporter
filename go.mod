191355c2803c6df59ad87d71f76b47af89e82052
sotirios.alfonsos <sotirios.alfonsos@ing.com>
2019-10-21 15:27:00 +0200

    Change scheduling lib to gocron, to support weekday scheduling

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
+	github.com/jasonlvhit/gocron v0.0.0-20191007145845-57f89394836a
 	gopkg.in/yaml.v2 v2.2.4
 )
--- scheduler.go
+++ scheduler.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"runtime"
 	"log"
 	"flag"
-	"github.com/carlescere/scheduler"
+	"github.com/jasonlvhit/gocron"
 	"oscap-report-exporter/oscap"
 )
 
@@ -14,9 +13,50 @@ func main() {
     flag.Parse()
 
 	startScheduler(*configFile)
+	// config := oscap.GetConfig(*configFile)
 
-	//Prevent job from exiting. Is that the right approvach?
-	runtime.Goexit()
+	// log.Printf("Starting Scheduler for " + config.ScanDate + " " + config.ScanTime)
+	// log.Printf("Working folder " + config.WorkingFolder)
+	// log.Printf("Global vulnerability report url " + config.VulnerabilityReportConf.BaseVulnerabilityReportUrl + config.VulnerabilityReportConf.GlobalVulnerabilityReportHttpsLocation)
+	
+	// if config.VulnerabilityReportConf.UserName != "" && config.VulnerabilityReportConf.Password != ""{
+	// 	log.Printf(config.VulnerabilityReportConf.UserName)
+	// 	log.Printf(config.VulnerabilityReportConf.Password)
+	// }	
+	// scheduler.Every().Day().At("14:34").Run(job)
+	// gocron.Every(1).Monday().At("15:17").Do(job)
+	// gocron.Every(1).Tuesday().At("14:40").Do(job)
+	// gocron.Every(1).Wednesday().At("14:40").Do(job)
+	// gocron.Every(1).Thursday().At("14:40").Do(job)
+	// gocron.Every(1).Friday().At("14:40").Do(job)
+	// gocron.Every(1).Saturday().At("14:40").Do(job)
+	// gocron.Every(1).Sunday().At("14:40").Do(job)
+
+	// <- gocron.Start()
+	//var job *scheduler.Job
+	// switch config.ScanDate {   
+	// 	case "Mon":
+	// 		scheduler.Every().Monday().At("11:04").Run(config.OscapVulnerabilityScan)
+	// 	// case "Tue":
+	// 	// 	job := scheduler.Every().Tuesday()
+	// 	// case "Wed":
+	// 	// 	job := scheduler.Every().Wednesday()
+	// 	// case "Thu":
+	// 	// 	job := scheduler.Every().Thursday()
+	// 	// case "Fri":
+	// 	// 	job := scheduler.Every().Friday()
+	// 	// case "Sat":
+	// 	// 	job := scheduler.Every().Saturday()
+	// 	// case "Sun":
+	// 	// 	log.Printf("Chose Sunday")
+	// 	// 	job := scheduler.Every().Sunday()
+	// 	// case "Daily":
+	// 	// 	job := scheduler.Every().Day()
+	// 	default:
+	// 		log.Fatalf("Scheduling option not supported")
+	// }
+	//Prevent job from exiting. Is that the right approach?
+	//runtime.Goexit()
 }
 
 func startScheduler(configFile string) {
@@ -24,34 +64,37 @@ func startScheduler(configFile string) {
 	config := oscap.GetConfig(configFile)
 
 	log.Printf("Starting Scheduler for " + config.ScanDate + " " + config.ScanTime)
-	log.Printf(config.WorkingFolder)
-	log.Printf(config.VulnerabilityReportConf.GlobalVulnerabilityReportHttpsLocation)
-	log.Printf(config.VulnerabilityReportConf.UserName)
-	log.Printf(config.VulnerabilityReportConf.Password)
-	log.Printf(config.VulnerabilityReportConf.BaseVulnerabilityReportUrl)
+	log.Printf("Working folder " + config.WorkingFolder)
+	log.Printf("Global vulnerability report url " + config.VulnerabilityReportConf.BaseVulnerabilityReportUrl + config.VulnerabilityReportConf.GlobalVulnerabilityReportHttpsLocation)
+	
+	if config.VulnerabilityReportConf.UserName != "" && config.VulnerabilityReportConf.Password != ""{
+		log.Printf(config.VulnerabilityReportConf.UserName)
+		log.Printf(config.VulnerabilityReportConf.Password)
+	}	
 
-	var job *scheduler.Job
+	job := &gocron.Job{}
 	switch config.ScanDate {   
 		case "Mon":
-			job = scheduler.Every().Monday()
+			job = gocron.Every(1).Monday()
 		case "Tue":
-			job = scheduler.Every().Tuesday()
+			job = gocron.Every(1).Tuesday()
 		case "Wed":
-			job = scheduler.Every().Wednesday()
+			job = gocron.Every(1).Wednesday()
 		case "Thu":
-			job = scheduler.Every().Thursday()
+			job = gocron.Every(1).Thursday()
 		case "Fri":
-			job = scheduler.Every().Friday()
+			job = gocron.Every(1).Friday()
 		case "Sat":
-			job = scheduler.Every().Saturday()
+			job = gocron.Every(1).Saturday()
 		case "Sun":
-			job = scheduler.Every().Sunday()
+			job = gocron.Every(1).Sunday()
 		case "Daily":
-			job = scheduler.Every().Day()
+			job = gocron.Every(1).Day()
 		default:
 			log.Fatalf("Scheduling option not supported")
 	}
-	job.At(config.ScanTime).Run(config.OscapVulnerabilityScan)
 
+	job.At(config.ScanTime).Do(config.OscapVulnerabilityScan)
+	<- gocron.Start()
 }
 
